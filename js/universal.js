caa71f860484d0196f65b7a2a74f98d459980f38
dscottclark <scottfoto@gmail.com>
2021-01-26 13:40:40 -0500

    latest test edits

--- js/universal.js
+++ js/universal.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $(function(){
-        $("#header").load("../universal/header.html");
-        $("#social").load("../universal/footer.html");
-    });
+    console.log ("loaded")
+        $("#header").load("universal/header.html");
+        $("#social").load("universal/footer.html");
 });
