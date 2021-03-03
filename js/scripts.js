e5e6bbff83b8aeffe7ff4f60146fcfbe9f80c08f
dscottclark <scottfoto@gmail.com>
2021-03-03 15:28:52 -0700

    homepage update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 /*!
     * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
     * Copyright 2013-2020 Start Bootstrap
@@ -30,6 +31,8 @@
         }
     });
 
+   
+
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
@@ -54,3 +57,4 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
